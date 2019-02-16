77e4d6be516cbc57a7cfe02741a0901dbf9b36d7
dashanchik <https://github.com/Dashanchik>
2019-02-16 15:05:16 +0300

    jenkins + allure test

---
+++ src/main/java/lesson6/SelenideIndexPage.java
@@ -0,0 +1,135 @@
+package lesson6;
+
+import base.DataProviders.ServiceMenuItems;
+import base.SelenideBase;
+import base.enums.Users;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.page;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class SelenideIndexPage extends SelenideBase {
+
+    public static final String PAGE_TITLE = "Home Page";
+
+    @FindBy(id = "user-icon")
+    private SelenideElement loginIcon;
+
+    @FindBy(css = "[id='name']")
+    private SelenideElement loginField;
+
+    @FindBy(css = "[id='password']")
+    private SelenideElement passwordField;
+
+    @FindBy(css = "[id='login-button']")
+    private SelenideElement submitButton;
+
+    public SelenideElement getUserName() {
+        return userName;
+    }
+
+    @FindBy(css = "[id='user-name']")
+    private SelenideElement userName;
+
+    @FindBy(css = "[class='uui-navigation nav navbar-nav m-l8']>li")
+    private List<SelenideElement> headerMenuItems;
+
+    @FindBy(css = "[class='dropdown']")
+    private SelenideElement servicesDropdownHeaderMenuItem;
+
+    @FindBy(css = "[class='dropdown-menu")
+    private SelenideElement servicesDropdownHeaderMenuList;
+
+    @FindBy(css = "[class^='icons-benefit']")
+    private List<SelenideElement> benefitIcons;
+
+    @FindBy(css = "[class='row clerafix benefits'] .benefit-txt")
+    private List<SelenideElement> benefitIconsText;
+
+    @FindBy(name = "main-title")
+    private SelenideElement mainHeader;
+
+    @FindBy(name = "jdi-text")
+    private SelenideElement textUnderHeader;
+
+    @FindBy(css = "[src='https://epam.github.io/JDI/index.html']")
+    private SelenideElement iFrame;
+
+    @FindBy(xpath = "//*[@id='epam_logo']")
+    private SelenideElement epamLogo;
+
+    @FindBy(css = "[class='text-center']>a")
+    private SelenideElement subHeader;
+
+    @FindBy(name = "navigation-sidebar")
+    private SelenideElement sidebarMenu;
+
+    @FindBy(css = "[class^='footer']")
+    private SelenideElement footer;
+
+    public SelenideIndexPage() {
+        page(this);
+    }
+
+    @Step("Login")
+    public void login(Users user) {
+        loginIcon.click();
+        loginField.sendKeys(user.login);
+        passwordField.sendKeys(user.password);
+        submitButton.click();
+        assertEquals(user.name, userName.getText());
+    }
+
+    // TODO This method should be parametrized - wasn't in the task
+    @Step("Check that service dropdown is present in header menu")
+    public void checkServicesDropdownMenuInHeader() {
+        SelenideElement servicesDropdownHeaderMenu = headerMenuItems.get(2);
+        Collection<SelenideElement> servicesHeaderElements;
+        Collection<String> serviceHeaderElementsTexts;
+        List<String> serviceMenuItems;
+        // TODO Please look to the IDEA warning - fixed
+        servicesDropdownHeaderMenu.click();
+        servicesHeaderElements = servicesDropdownHeaderMenu.findAll(By.cssSelector("ul>li"));
+        serviceHeaderElementsTexts = servicesHeaderElements.stream().map(SelenideElement::getText).collect(Collectors.toList());
+        serviceMenuItems = Arrays.stream(ServiceMenuItems.values()).map(ServiceMenuItems::toString).collect(Collectors.toList());
+        assertTrue(serviceHeaderElementsTexts.containsAll(serviceMenuItems));
+    }
+
+    // TODO This method should be parametrized - wasn't in the task
+    @Step("Check that service dropdown is present in sidebar menu")
+    public void checkServicesDropdownMenuInSidebar() {
+        SelenideElement servicesDropdownSidebarMenu = sidebarMenu.find(By.cssSelector("[index='3']"));
+        Collection<SelenideElement> servicesSidebarElements;
+        Collection<String> servicesSidebarElementsTexts;
+        List<String> serviceMenuItems;
+        servicesDropdownSidebarMenu.click();
+        servicesSidebarElements = servicesDropdownSidebarMenu.findAll(By.cssSelector("ul>li"));
+        servicesSidebarElementsTexts = servicesSidebarElements.stream().map(element -> element.getText().toUpperCase()).collect(Collectors.toList());
+        serviceMenuItems = Arrays.stream(ServiceMenuItems.values()).map(ServiceMenuItems::toString).collect(Collectors.toList());
+        assertTrue(servicesSidebarElementsTexts.containsAll(serviceMenuItems));
+    }
+
+    // TODO This method should be parametrized - wasn't in the task
+    @Step("Go to the Different elements page from header menu")
+    public void openDifferentElementsPage() {
+        servicesDropdownHeaderMenuItem.click();
+        servicesDropdownHeaderMenuList.find(byText("Different elements")).click();
+    }
+
+    // TODO This method should be parametrized - wasn't in the task
+    @Step("Go to the Dates page from header menu")
+    public void openDatesPage() {
+        servicesDropdownHeaderMenuItem.click();
+        servicesDropdownHeaderMenuList.find(byText("Dates")).click();
+    }
+}
---
+++ src/test/java/lesson6/steps/NavigationStep.java
@@ -0,0 +1,12 @@
+//package lesson6.steps;
+//
+//import cucumber.api.PendingException;
+//
+//public class NavigationStep {
+//    public NavigationStep() {
+//        Given("^I open EPAM JDI site$", () -> {
+//            // Write code here that turns the phrase above into concrete actions
+//            throw new PendingException();
+//        });
+//    }
+//}
