e1efa2b9a5df072a0e50dd576b3f37e132c58e1e
dashanchik <https://github.com/Dashanchik>
2019-03-05 23:50:40 +0300

    HW7 correction (2/2)

--- src/main/java/base/DataProviders/MetalsAndColorsDataProvider.java
+++ src/main/java/base/DataProviders/MetalsAndColorsDataProvider.java
@@ -1,16 +1,15 @@
 package base.DataProviders;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import org.testng.annotations.DataProvider;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class MetalsAndColorsDataProvider {
 
@@ -20,15 +19,14 @@ public class MetalsAndColorsDataProvider {
         JsonReader reader = new JsonReader(new FileReader("src/test/resources/JDI_ex8_metalsColorsDataSet.json"));
         //parse the file into JsonObject
         JsonObject fileData = (JsonObject) new JsonParser().parse(reader);
-        //create list of JsonElements from the JsonOblect and fill it with json values for each json file data obj - JsonElement
-        List<JsonElement> listOfJsons = fileData.keySet().stream().map(fileData::get).collect(Collectors.toList());
-        //map the created list of JsonElements on MetalsAndColorsTestData class fields
-        List<MetalsAndColorsTestData> listOfTestData = listOfJsons.stream().map(e -> new Gson().fromJson(e, MetalsAndColorsTestData.class)).collect(Collectors.toList());
+        //map the created Json on MetalsAndColorsTestData class fields using Map<String, MetalsAndColorsTestData>
+        Map<String, MetalsAndColorsTestData> listOfTestData = new Gson().fromJson(fileData, new TypeToken<Map<String, MetalsAndColorsTestData>>() {
+        }.getType());
         //fill in the dataProvider Object[][] with the testdata (1 stands for MetalsAndColorsTestData obj)
         Object[][] finalData = new Object[listOfTestData.size()][1];
-        int index = 0;
-        for (Object[] each : finalData) {
-            each[0] = listOfTestData.get(index++);
+        for (int index = 0; index < finalData.length; index++) {
+            //fill the finalData[][] with the values from map listOfTestData
+            finalData[index][0] = listOfTestData.get(listOfTestData.keySet().toArray()[index]);
         }
         return finalData;
     }
